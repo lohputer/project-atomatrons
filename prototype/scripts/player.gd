44b09d172a8c059ece23799fb81feff2d6e77bc3
lohputer <leynalyndon@gmail.com>
2024-04-12 17:38:20 +0800

    I CANT FIGURE OUT HOW TO ROTATE A CHARACTER 2D HORIZONTALLY
    oh i guess i shrank the platyer
    - Lyndon

--- prototype/scripts/player.gd
+++ prototype/scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 var player_direction: Vector2
 func _physics_process(delta):
 	player_direction = Input.get_vector("left", "right", "up", "down")
-	
 	velocity = player_direction * move_Speed
+	if player_direction[0] > 0: #right
+		pass
+	elif player_direction[0] < 0: 
+		pass
 	move_and_slide()
