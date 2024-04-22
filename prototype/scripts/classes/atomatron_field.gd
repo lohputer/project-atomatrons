d06415597a1ea8ffb1341e81fbbb50db7a4abab1
rizzardoflegend <rizzardoflegend@gmail.com>
2024-04-22 18:01:06 +0800

    Update atomatron_field.gd

--- prototype/scripts/classes/atomatron_field.gd
+++ prototype/scripts/classes/atomatron_field.gd
@@ -3,7 +3,7 @@ class_name atomatron_field
 @export var max_health: int #max health
 var health: int 
 @export var atomatron_battle_path: String #put the path of the fighting atomatron in here
-#@export var status_effect
+@export var status_effect_permanent: status_effect
 func _process(delta):
 	if health > max_health:
 		health = max_health
