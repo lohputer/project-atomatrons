9429a7c5aef18f00cb50b59cc94d3ea230518770
rizzardoflegend <rizzardoflegend@gmail.com>
2024-04-23 19:57:18 +0800

    hur hurhruhru

--- prototype/prefabs/berries/berries.gd
+++ prototype/prefabs/berries/berries.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends food
 
 var playerInRange = false
 
---
+++ prototype/scripts/player_info.gd
@@ -0,0 +1,8 @@
+extends Node
+var carbs : int
+var proteins : int
+var fats : int
+var vitC : int
+var vitD : int
+var calcium : int
+var iron : int
