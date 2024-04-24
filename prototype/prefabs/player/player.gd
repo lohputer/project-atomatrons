ceb4f1ab452c199c6fdd2cd696569f24db6f464a
lohputer <leynalyndon@gmail.com>
2024-04-24 15:13:43 +0800

    Timer for digesting stats needs to be fixed

--- prototype/prefabs/player/player.gd
+++ prototype/prefabs/player/player.gd
@@ -18,6 +18,14 @@ var player_direction: Vector2
 
 func _ready():
 	PlayerInfo.player_node = self
+	PlayerInfo.carbs = 100
+	PlayerInfo.fats = 60
+	PlayerInfo.proteins = 80
+	PlayerInfo.iron = 20
+	PlayerInfo.calcium = 20
+	PlayerInfo.vitD = 15
+	PlayerInfo.vitC = 30
+	
 func _physics_process(delta):
 	player_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = player_direction * PlayerInfo.player_speed
@@ -27,11 +35,19 @@ func _physics_process(delta):
 		$Untitled.flip_h = false
 	move_and_slide()
 	PlayerInfo.player_pos = global_position
+	await get_tree().create_timer(10).timeout
+	digest()
+	await get_tree().create_timer(10).timeout #fix timer send help
 	
-
-
-
-
+func digest():
+	print(PlayerInfo.carbs, PlayerInfo.proteins, PlayerInfo.fats, PlayerInfo.vitC, PlayerInfo.vitD, PlayerInfo.iron, PlayerInfo.calcium)
+	PlayerInfo.carbs -= 3
+	PlayerInfo.proteins -= 3
+	PlayerInfo.fats -= 2
+	PlayerInfo.vitC -= 1
+	PlayerInfo.vitD -= 1
+	PlayerInfo.calcium -= 1
+	PlayerInfo.iron -= 1
 
 func _on_area_2d_body_entered(body):
 	if body is atomatron_field:
