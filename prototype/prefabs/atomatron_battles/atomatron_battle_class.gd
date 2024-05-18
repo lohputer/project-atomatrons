69b6c5d67ebb8b2eb58f7cd0daae3d2fbff1c999
rizzardoflegend <rizzardoflegend@gmail.com>
2024-05-18 16:04:27 +0800

    combat (wip)

---
+++ prototype/prefabs/atomatron_battles/atomatron_battle_class.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+@export var max_health: int
+var health: int
+@export var speed: int
+
---
+++ prototype/prefabs/atomatron_battles/move_class.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name move_info
+@export var move_name: String
+@export var move_damage: float
+@export var move_effect: String
+@export var move_type: String
