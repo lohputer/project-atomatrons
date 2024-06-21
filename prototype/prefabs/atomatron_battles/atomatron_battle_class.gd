e308f0d7199726661046b430c80a8af73339da83
rizzardoflegend <rizzardoflegend@gmail.com>
2024-06-21 12:10:24 +0800

    wahh

--- prototype/prefabs/atomatron_battles/atomatron_battle_class.gd
+++ prototype/prefabs/atomatron_battles/atomatron_battle_class.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
+class_name atomatron_battle
 @export var max_health: int
 var health: int
 @export var speed: int
 
+func get_hit(damage: move_info):
+	health -= damage.move_damage
+	await get_tree().create_timer(10).timeout
+	
