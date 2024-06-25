9ed3840715fc84c2f4cb38f55ab5035b09a4470d
rizzardoflegend <rizzardoflegend@gmail.com>
2024-06-25 23:29:59 +0800

    battle system is the storm that is approaching

--- prototype/scenes/combat_scene.gd
+++ prototype/scenes/combat_scene.gd
@@ -3,3 +3,10 @@ extends Node2D
 
 func _on_button_pressed():
 	GlobalFunctions.load_to("res://scenes/test_field.tscn")
+
+func turn_manage():
+	# sort nodes here
+	for i in $entities.get_child(2):
+		if i.speed > $entities.get_child(1).speed:
+			pass
+	
---
+++ prototype/scenes/player.gd
@@ -0,0 +1,21 @@
+extends atomatron_battle
+
+func turn():
+	$"../../player choices".visible = true
+
+
+func _on_attack_pressed():
+	pass # attack
+	var newdamage = move_info.new()
+	newdamage.move_damage = 5
+	newdamage.move_name = "strike"
+	newdamage.move_effect = "non"
+	target.get_hit(newdamage)
+
+
+func _on_speed_up_pressed():
+	pass # speed up
+
+
+func _on_defence_up_pressed():
+	pass # defence up
