d6e4b23fab07e089693304c34ee64f2833d83953
rizzardoflegend <163544065+rizzardoflegend@users.noreply.github.com>
2024-06-28 16:06:01 +0800

    no worky thing

--- prototype/scenes/enemy.gd
+++ prototype/scenes/enemy.gd
@@ -1,5 +1,7 @@
 extends atomatron_battle
 func turn():
+	await get_tree().create_timer(5).timeout
+	get_parent().get_parent().turn_finished = true
 	pass
 func _process(delta):
 	$"../../enemy health".text = str($".".health)
