5a7444335037b0a58ed8f9c02e56421c16f554f9
MrSuperSquishy <221195L@student.hci.edu.sg>
2024-07-10 10:30:06 +0800

    Ms pek yap session
    I added chlorine, lithium and magnesium and Im pushing it now because I dont want to lose all my progress again.

--- prototype/scenes/test_field.gd
+++ prototype/scenes/test_field.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+#
 var nitrogen_scene = preload("res://prefabs/atomatrons/atomic/nitrogen/nitrogen_field.tscn")
 var food_scene = preload("res://prefabs/item/item.tscn")
 var spawn_timer = Timer.new()
--- prototype/scripts/classes/atomatron_field.gd
+++ prototype/scripts/classes/atomatron_field.gd
@@ -4,9 +4,8 @@ class_name atomatron_field
 var health: int 
 @export var atomatron_battle_path: String #put the path of the fighting atomatron in here
 @export var status_effect_permanent: status_effect
+
 func _process(delta):
 	if health > max_health:
 		health = max_health
 	
-
-
