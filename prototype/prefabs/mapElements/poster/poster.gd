7c8fc8c17b4c1755255492848fcd84dcbbf9cb17
lohputer <leynalyndon@gmail.com>
2024-07-13 14:32:10 +0800

    I MADE IT GTG NOW

--- prototype/prefabs/mapElements/poster/poster.gd
+++ prototype/prefabs/mapElements/poster/poster.gd
@@ -1,29 +1,35 @@
 extends Node2D
+
 @export var posterContent = ""
+var posterNode = preload("res://prefabs/mapElements/poster/poster_content.tscn")
 const contentDict = {
 	"DIFFUSION" : preload("res://assets/posters/diffusion.PNG"),
 	"RESPIRATORY": preload("res://assets/posters/respiration.PNG"),
 	"ROOT": preload("res://assets/posters/rootPoster.PNG"),
 }
 var in_radius = false
+var instantiatedPosterNode = posterNode.instantiate()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if posterContent:
-		$posterContent.get_child(0).texture = contentDict[posterContent]
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("use_item") and in_radius:
-		$posterContent.position = Vector2(0, 0)
-		$posterContent.visible = true
-		$posterContent.z_index = 3
-		$"../player".add_child($posterContent)
+		if posterContent:
+			instantiatedPosterNode.get_child(0).texture = contentDict[posterContent]
+		instantiatedPosterNode.position = Vector2(0, 0)
+		instantiatedPosterNode.visible = true
+		instantiatedPosterNode.z_index = 3
+		$"../player".add_child(instantiatedPosterNode)
 
 func _on_area_2d_body_entered(body):
-	if body.id == "player":
+	if body.name == "player":
 		in_radius = true
 
 func _on_area_2d_body_exited(body):
-	if body.id == "player":
+	if body.name == "player":
 		in_radius = false
+		instantiatedPosterNode.queue_free()
+		instantiatedPosterNode = posterNode.instantiate()
