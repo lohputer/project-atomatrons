b2203923d1cecee124e5eb0af24ddb26e7be798c
lohputer <leynalyndon@gmail.com>
2024-07-13 21:25:44 +0800

    QUIZ TXT WORKING PART ONE

--- prototype/prefabs/mapElements/npc/npc.gd
+++ prototype/prefabs/mapElements/npc/npc.gd
@@ -2,12 +2,30 @@ extends Node2D
 
 @export_file var dialogueFileName = "res://dialouge/baseDialogue.dialogue"
 @export_file var imageFileName = "res://icon.svg"
-
-var dialogueFile;
+var dialogueFile
+var questions = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	dialogueFile = load(dialogueFileName)
+	var file = FileAccess.open("res://dialouge/quiz.txt", FileAccess.READ)
+	var content = file.get_as_text()
+	content = content.split("TOPIC ")
+	for topic in content:
+		var lines = topic.split("\nquestion: ")
+		if lines[0]:
+			questions[lines[0]] = []
+		else:
+			continue
+		var unsplitted = lines.slice(1,)
+		for q in unsplitted:
+			var newList = []
+			var question = q.split("\noptions: ")[0]
+			var options = q.split("\noptions: ")[1].split("\ncorrect: ")[0]
+			options = options.split(", ")
+			var correct = q.split("\noptions: ")[1].split("\ncorrect: ")[1].split("\n")[0]
+			questions[lines[0]].append([question, options, correct])
+	print(questions["1"])
 	var texture = load(imageFileName)
 	$Sprite2D.texture = texture
 	
