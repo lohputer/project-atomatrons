fcaaba4df4bb5659db29aba6493d3aaaf1871c8a
lohputer <leynalyndon@gmail.com>
2024-07-17 09:27:26 +0800

    The NPC question mechanic HAS FLOURISHED

--- prototype/prefabs/mapElements/npc/npc.gd
+++ prototype/prefabs/mapElements/npc/npc.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export_file var dialogueFileName = "res://dialouge/baseDialogue.dialogue"
 @export_file var imageFileName = "res://icon.svg"
-@export var topic = 0
+@export var topic: String = ""
 @export var qNum = 1
 var dialogueFile
 var questions = {}
@@ -16,7 +16,11 @@ func _ready():
 	var content = file.get_as_text()
 	content = content.split("TOPIC ")
 	for topic in content:
-		var lines = topic.split("\nquestion: ")
+		var lines = []
+		if "question: " in topic:
+			lines = topic.split("\nquestion: ")
+		else:
+			lines = topic.split("\nQuestion: ")
 		if lines[0]:
 			questions[lines[0]] = []
 		else:
@@ -24,16 +28,22 @@ func _ready():
 		var unsplitted = lines.slice(1,)
 		for q in unsplitted:
 			var newList = []
-			var question = q.split("\noptions: ")[0]
-			var options = q.split("\noptions: ")[1].split("\ncorrect: ")[0]
-			options = options.split(", ")
-			var correct = q.split("\noptions: ")[1].split("\ncorrect: ")[1].split("\n")[0]
+			var question = ""
+			var options = []
+			var correct = ""
+			if "options" in q:
+				question = q.split("\noptions: ")[0]
+				options = q.split("\noptions: ")[1].split("\ncorrect: ")[0]
+				options = options.split(", ")
+				correct = q.split("\noptions: ")[1].split("\ncorrect: ")[1].split("\n")[0]
+			else:
+				question = q.split("\nOptions: ")[0]
+				options = q.split("\nOptions: ")[1].split("\nCorrect: ")[0]
+				options = options.split(", ")
+				correct = q.split("\nOptions: ")[1].split("\nCorrect: ")[1].split("\n")[0]
 			if options.find(correct,0) != -1:
 				correct = options.find(correct,0)
 			questions[lines[0]].append([question, options, correct])
-	print(questions["1"])
-	if topic:
-		questions = questions[topic]
 	var texture = load(imageFileName)
 	$Sprite2D.texture = texture
 	
@@ -41,17 +51,28 @@ func _ready():
 func _process(delta):
 	pass
 
-func pick_random(dict, num):
-	if len(dict) > 1:
-		var splitLst = dict[dict.keys()[randi() % len(dict)]]
-		pick_random(splitLst, num)
-	else:
-		var qs = []
-		for i in range(num):
-			qs.append(dict[randi() % len(dict)])
-		return qs
+func pick_random(dict, num, topic):
+	var qs = []
+	for i in range(num):
+		var lst = []
+		if not topic:
+			lst = dict[dict.keys()[randi() % len(dict)]]
+		else:
+			lst = dict[topic]
+		var rand = randi() % len(lst)
+		while lst[rand] in qs:
+			rand = randi() % len(lst)
+		qs.append(lst[rand])
+	return qs
 
 func _on_area_2d_body_entered(body):
-	GameState.question = pick_random(questions, qNum)
-	DialogueManager.show_example_dialogue_balloon(dialogueFile, "starting_node")
+	if body.id == "player":
+		var q = pick_random(questions, qNum, topic)
+		GameState.question = q[0][0]
+		GameState.option1 = q[0][1][0]
+		GameState.option2 = q[0][1][1]
+		GameState.option3 = q[0][1][2]
+		GameState.option4 = q[0][1][3]
+		GameState.correct = int(q[0][2])
+		DialogueManager.show_example_dialogue_balloon(dialogueFile, "starting_node")
 
--- prototype/scripts/game_state.gd
+++ prototype/scripts/game_state.gd
@@ -1,3 +1,8 @@
 extends Node
 
 var question
+var option1
+var option2
+var option3
+var option4
+var correct
