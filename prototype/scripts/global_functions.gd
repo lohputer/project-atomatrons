2baf66ad1fae5b170a9c090737b6c0498b4641a3
rizzardoflegend <rizzardoflegend@gmail.com>
2024-07-30 16:32:20 +0800

    sigma

--- prototype/scripts/global_functions.gd
+++ prototype/scripts/global_functions.gd
@@ -1,7 +1,8 @@
 extends Node
-
+#global script so sigma
 var player_atomatron_deployable: String
 var enemy_atomatron_deployable: String
+var move_announced: String
 func load_to(target: String):
 	var loading_Screen = preload("res://prefabs/loading_screen.tscn").instantiate()
 	loading_Screen.next_scene_path = target
@@ -11,3 +12,6 @@ func load_combat(player_atomatron: String, enemy_atomatron: String):
 	player_atomatron_deployable = player_atomatron
 	enemy_atomatron_deployable = enemy_atomatron
 	load_to("res://scenes/test_combat.tscn")
+
+func dialogue_move(move_name: String):
+	move_announced = move_name
